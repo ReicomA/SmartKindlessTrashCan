29475e554fb8190ad8334e9483461c4acfee9340
ReicomA <seokbong60@gmail.com>
2020-08-12 20:49:07 +0900

    make motor

--- api/driver/motor/cover_motor.py
+++ api/driver/motor/cover_motor.py
@@ -1,4 +1,19 @@
 from motor import Motor
+from RPI.GPIO as GPIO
 
 class CoverMotor(Motor):
-    pass
+    def __init__(self, pin):
+        self.motor = None
+
+        #GPIO Set
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(pin, GPIO.OUT)
+
+        self.motor = GPIO.PWM(pin, 50)
+        self.motor.start(0)
+
+    def left(self):
+        self.motor.ChangeDutyCircle(2)
+    
+    def right(self):
+        self.motor.ChangeDutyCircle(9)
